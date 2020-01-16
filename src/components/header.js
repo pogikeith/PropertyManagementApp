09ce1a0041db04d0f432cbdcb8b3a50f6d840c35
pogikeith <pogikeith@yahoo.com>
2020-01-16 10:44:45 -0700

    firsst commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Header Component</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default header
